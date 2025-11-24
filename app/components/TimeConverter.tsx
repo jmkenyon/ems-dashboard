aca2463f4ac85ecc0ff54b3c2b0e6587de48a7ee
Josh Kenyon <joshuakenyon1@gmail.com>
2025-11-24 17:53:59 -0300

    font sizes

--- app/components/TimeConverter.tsx
+++ app/components/TimeConverter.tsx
@@ -76,12 +76,12 @@ const TimeConverter = () => {
     <div className=" flex flex-col justify-center items-center py-20">
       <div className="bg-white  text-blue-950 sm:p-20 p-10 flex flex-col rounded-2xl shadow-2xl">
         <h1 className="sm:text-3xl text-lg font-bold mb-6">Time Stamp Converter</h1>
-        <p className="text-gray-500 pb-10 sm:text-md text-sm">
+        <p className="text-gray-500 pb-10 sm:text-base text-sm">
           Enter the prefix of the server and the timestamp you are looking to
           convert
         </p>
         <form onSubmit={handleSubmit} className="flex flex-col gap-6">
-          <label className="text-sm sm:text-md">
+          <label className="text-sm sm:text-base">
             Choose a server:
             <select
               className="ml-4"
@@ -94,7 +94,7 @@ const TimeConverter = () => {
               <option value="LD6">LD6</option>
             </select>
           </label>
-          <label className="flex flex-row gap-3 text-sm sm:text-md">
+          <label className="flex flex-row gap-3 text-sm sm:text-base">
             Enter a timestamp:
             <input
               name="timestamp"
