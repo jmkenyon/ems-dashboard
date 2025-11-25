3f17392a81d9d849d7af0605dffff18f48c0065c
Josh Kenyon <joshuakenyon1@gmail.com>
2025-11-25 16:48:29 -0300

    more shadow

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col justify-center items-center p-10 mt-20">
-      <div className="bg-white shadow-2xl rounded-2xl max-w-3xl text-center p-15">
+      <div className="bg-white shadow-2xl rounded-2xl max-w-3xl text-center p-15 shadow-black/50">
         <h1 className="text-5xl font-bold text-blue-950 mb-15">
           Welcome to EMS Tool Suite
         </h1>
