4c34dd5f462088288a1ddeee4392bd2cae4b309a
Josh Kenyon <joshuakenyon1@gmail.com>
2025-11-28 18:22:49 -0300

    api maybe in prod

--- app/holiday-checker/sendToBackend.ts
+++ app/holiday-checker/sendToBackend.ts
@@ -1,7 +1,10 @@
+
+const API_URL = process.env.NEXT_PUBLIC_PYTHON_BACKEND;
+
 export async function sendHolidaysToBackend(
   rows: Array<{ market: string; date: string }>
 ) {
-  const response = await fetch("http://127.0.0.1:8000/check-holiday", {
+  const response = await fetch(`${API_URL}/check-holiday`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
